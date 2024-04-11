51ee31fe001a33f007055b8400dafc4d67a7e90b
Chethiya2001 <chethiyabandaraherath@gmail.com>
2024-04-11 15:19:28 +0530

    start_about_us

---
+++ lib/features/about_us/screens/about_us_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AboutUsScreen extends ConsumerStatefulWidget {
+  const AboutUsScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _AboutUsScreenState();
+}
+
+class _AboutUsScreenState extends ConsumerState<AboutUsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
