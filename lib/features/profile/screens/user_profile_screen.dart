28ab3d16b5b9202e057c1de0f4f6d154d4bc00b3
Chethiya2001 <chethiyabandaraherath@gmail.com>
2024-04-15 14:31:08 +0530

    user_profile_part_one

---
+++ lib/features/profile/screens/user_profile_screen.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  final userProfileKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEAEBED),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+          child: GNav(
+              backgroundColor: Colors.white,
+              color: Colors.black,
+              activeColor: Theme.of(context).colorScheme.secondary,
+              curve: Curves.linearToEaseOut,
+              tabBorderRadius: 10,
+              tabBackgroundColor:
+                  Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+              gap: 8,
+              padding: EdgeInsets.all(16),
+              textStyle: TextStyle(
+                  color:
+                      Colors.black), // the tab button gap between icon and text
+
+              tabs: [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                GButton(
+                  icon: Icons.shopping_basket,
+                  text: 'Cart',
+                ),
+                GButton(
+                  icon: Icons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: Icons.people,
+                  text: 'Profile',
+                )
+              ]),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 330,
+              child: Stack(
+                children: [
+                  SizedBox(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondary,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(60),
+                          bottomRight: Radius.circular(60),
+                        ),
+                      ),
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.3,
+                    ),
+                  ),
+                  // profile and Text
+                  // Positioned(
+                  //   top: MediaQuery.of(context).size.height * 0.1,
+                  //   left: MediaQuery.of(context).size.width * 0.2,
+                  //   child: Row(
+                  //     crossAxisAlignment: CrossAxisAlignment.center,
+                  //     children: [
+                  //       CircleAvatar(
+                  //         radius: 40,
+                  //         backgroundImage: AssetImage('assets/profile.avif'),
+                  //       ),
+                  //       SizedBox(width: 10),
+                  //       Column(
+                  //         children: [
+                  //           Text('John Smith',
+                  //               textAlign: TextAlign.start,
+                  //               style:
+                  //                   Theme.of(context).textTheme.displayMedium),
+                  //           SizedBox(height: 5),
+                  //           Text(
+                  //             'example@gmail.com',
+                  //             textAlign: TextAlign.start,
+                  //             style: Theme.of(context).textTheme.labelSmall,
+                  //           ),
+                  //         ],
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  // Positioned Container
+                  Positioned(
+                    top: MediaQuery.of(context).size.height * 0.23,
+                    left: MediaQuery.of(context).size.width * 0.08,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            BorderRadius.all(Radius.elliptical(20, 20)),
+                      ),
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      height: MediaQuery.of(context).size.height * 0.12,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundImage: AssetImage('assets/profile.avif'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //form
+            Padding(
+              padding: const EdgeInsets.fromLTRB(22, 1, 22, 1),
+              child: Form(
+                key: userProfileKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: TextFormField(
+                          // controller: loginEmailauthController,
+                          decoration: const InputDecoration(
+                            labelText: 'Email.',
+                            labelStyle: TextStyle(
+                              color: Color.fromRGBO(0, 0, 0, 0.5),
+                            ),
+                            contentPadding: EdgeInsets.only(left: 20),
+                            prefixIcon: Opacity(
+                              opacity: 0.5,
+                              child: Icon(Icons.email_outlined),
+                            ),
+                            border: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                          ),
+                          textAlignVertical: TextAlignVertical.center,
+                          textCapitalization: TextCapitalization.none,
+                          keyboardType: TextInputType.emailAddress,
+                          style: const TextStyle(color: Colors.black),
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                !value.contains('@')) {
+                              return 'Please enter a valid email address.';
+                            }
+                            return null;
+                          }),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
